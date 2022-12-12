787b758cb2b90e718c3fc8aa06ef1d9cec3d7f1a
JuanCarlos <jcge97@gmail.com>
2022-12-12 12:37:25 +0000

    Mejorar los resultados que deberían ir en la memoria

--- Garcia_SnakeViral_2022_Nature/code/resultados_memoria.sh
+++ Garcia_SnakeViral_2022_Nature/code/resultados_memoria.sh
@@ -2,8 +2,8 @@
 ruta=../data/processed
 all_fasta=../data/processed/all_sequences.fasta
 
-grep ">SRR1984406.2835 2835 length=134" -A 1 $all_fasta > $ruta/secuencia1.fasta
-grep ">SRR1984406.5955 5955 length=134" -A 1 $all_fasta > $ruta/secuencia2.fasta
-grep ">SRR1984406.1377 1377 length=134" -A 1 $all_fasta > $ruta/secuencia3.fasta
-grep ">SRR1984406.2911 2911 length=135" -A 1 $all_fasta > $ruta/secuencia4.fasta
-grep ">SRR1984406.4139 4139 length=134" -A 1 $all_fasta > $ruta/secuencia5.fasta
+grep -Fw ">SRR1984406.2835 2835 length=134" -A 1 $all_fasta > $ruta/secuencia1.fasta
+grep -Fw ">SRR1984406.5955 5955 length=134" -A 1 $all_fasta > $ruta/secuencia2.fasta
+grep -Fw ">SRR1984406.1377 1377 length=134" -A 1 $all_fasta > $ruta/secuencia3.fasta
+grep -Fw ">SRR1984406.2911 2911 length=135" -A 1 $all_fasta > $ruta/secuencia4.fasta
+grep -Fw ">SRR1984406.4139 4139 length=134" -A 1 $all_fasta > $ruta/secuencia5.fasta
